8295d5ccf315a43489e7c83669a74d7dbbde9d14
DaniCis <danicisneros2@hotmail.com>
2022-11-06 22:37:15 -0500

    add navbar

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,56 @@
 <script>
     import Sidebar from "../components/sidebar.svelte" 
+    import Navbar from "../components/navbar.svelte"
     
 </script>
 
-<Sidebar />
+
+<div>
+    <Sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100">
+        <Navbar/>
+        <div class="relative bg-red-100 md:pt-32 pb-32 pt-12">
+            <div class="px-4 md:px-10 mx-auto w-full">
+                <div>
+                    <div class="flex flex-wrap">
+                        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                            <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                <div class="flex-auto p-4">
+                                    <div class="flex flex-wrap">
+                                        <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                            <h5 class="text-blueGray-400 uppercase text-xs">Compras</h5>
+                                            <span class="font-semibold text-xl text-blueGray-700">sdfgh</span>
+                                        </div>
+                                        <div class="relative w-auto pl-4 flex-initial">
+                                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full red">
+                                                i
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
+                            <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                                <div class="flex-auto p-4">
+                                    <div class="flex flex-wrap">
+                                        <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
+                                            <h5 class="text-blueGray-400 uppercase  text-xs">Ventas</h5>
+                                            <span class="font-semibold text-xl text-blueGray-700">sdfgh</span>
+                                        </div>
+                                        <div class="relative w-auto pl-4 flex-initial">
+                                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full red">
+                                                i
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
